3f7fe0331467bede95d56ea6c55fad4e1cf9d6d3
Florian Kofler <florian.sebastian.kofler@gmail.com>
2019-04-05 20:30:48 +0200

    p3.6 print and pep

--- s3.py
+++ s3.py
@@ -5,31 +5,32 @@ import time
 
 if __name__ == '__main__':
 
-	from sys import argv
-	myargs = utils.getopts(argv)
+    from sys import argv
 
-	want_tissues = False
-	want_atlas   = False
+    myargs = utils.getopts(argv)
 
-	if '-i' in myargs: 
-		input_path = myargs['-i']
-	        output_path = os.path.dirname(os.path.abspath(input_path))
+    want_tissues = False
+    want_atlas = False
 
-	if '-o' in myargs:
-		output_path = myargs['-o']
-                output_path = os.path.abspath(output_path)
-   	
-	if '-t' in myargs:
-		want_tissues = True
+    if '-i' in myargs:
+        input_path = myargs['-i']
+        output_path = os.path.dirname(os.path.abspath(input_path))
 
-	if '-a' in myargs:
-		want_atlas = True
+    if '-o' in myargs:
+        output_path = myargs['-o']
+        output_path = os.path.abspath(output_path)
 
-	if not os.path.exists(output_path):
-                print("The selected output folder doesn't exist, so I am making it \n")
-		os.makedirs(output_path)
-   
-        start = time.time()
-	skull_stripper = SkullStripper(input_path, output_path, want_tissues, want_atlas)
-	skull_stripper.strip_skull()
-        print 'Done (' + str((time.time() - start) / 60.) + ' min)'
+    if '-t' in myargs:
+        want_tissues = True
+
+    if '-a' in myargs:
+        want_atlas = True
+
+    if not os.path.exists(output_path):
+        print("The selected output folder doesn't exist, so I am making it \n")
+        os.makedirs(output_path)
+
+    start = time.time()
+    skull_stripper = SkullStripper(input_path, output_path, want_tissues, want_atlas)
+    skull_stripper.strip_skull()
+    print('Done (' + str((time.time() - start) / 60.) + ' min)')
